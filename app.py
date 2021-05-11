e25b27756c56a7cbe61eed57af9c82f29108846c
DanielAdeyemi <adeyemidany@gmail.com>
2021-05-11 16:07:30 -0700

    update app to use restful api in routes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, abort
+from flask import Flask, render_template, abort, jsonify
 from model import db
 
 app = Flask(__name__)
@@ -7,12 +7,23 @@ app = Flask(__name__)
 def welcome():
   return render_template(
     'welcome.html',
-    message="What's up")
+    cards=db)
 
 @app.route('/card/<int:index>')
 def card_view(index):
   try:
     card = db[index]
     return render_template("card.html", card=card, index=index, max_index=len(db)-1)
+  except IndexError:
+    abort(404)
+
+@app.route("/api/card/")
+def api_card_list():
+  return jsonify(db)
+
+@app.route("/api/card/<int:index>")
+def api_card_detail(index):
+  try:
+    return db[index]
   except IndexError:
     abort(404)
