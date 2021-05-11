92557f8117a1d77dee7cb2f62f99374c4bb59709
DanielAdeyemi <adeyemidany@gmail.com>
2021-05-11 12:50:47 -0700

    add counter and datetime routes

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 from flask import Flask
+from datetime import datetime
 
 app = Flask(__name__)
+
+counter = 0
+
 @app.route('/')
 def welcome():
   return 'Welcome to my page!'
+
+@app.route('/date')
+def date():
+  return "this page was served at " + str(datetime.now())
+
+@app.route('/num')
+def num():
+  global counter
+  counter += 1
+  return (f"this page was visited {counter} times")
